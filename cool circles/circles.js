2443efac22b60b2cf04ac89b0087f4bbb9247cb4
morkley1 <63617880+morkley1@users.noreply.github.com>
2025-07-10 11:53:34 -0400

    Add cool circles Files

---
+++ cool circles/circles.js
@@ -0,0 +1,202 @@
+const TYPES = {
+    'circle': {
+        'draw': (self) => {
+            doFill = false
+            doStroke = true
+            stroke('white')
+            strokeWeight(2)
+            circle(0, 0, self.size)
+        },
+        'openings': (self, size) => {
+            return [{
+                'size': size,
+                'position': new Vector(0, 0),
+                'rotation': 0,
+                'ocu': [0]
+            }]
+        }
+    },
+    'ngon': {
+        'draw': (self) => {
+            doFill = false
+            doStroke = true
+            stroke('white')
+            strokeWeight(2)
+            polygon(0, 0, self.size, self.n, self.j)
+        },
+        'openings': (self, size) => {
+            if (self.j > self.n) self.j = 1
+            let out = [{
+                    'size': size,
+                    'position': new Vector(0, 0),
+                    'rotation': 0,
+                    'ocu': [0, 1]
+                }]
+            let r = size * (Math.cos(PI * self.j / self.n) / Math.cos(PI / self.n))
+            let a = r * Math.sin(PI / self.n)
+            out.push({
+                'size': a * Math.cot(PI / self.n),
+                'position': new Vector(0, 0),
+                'rotation': PI / self.n,
+                'ocu': [0, 1]
+            })
+            out.push({
+                'size': r,
+                'position': new Vector(0, 0),
+                'rotation': PI / self.n,
+                'ocu': [2]
+            })
+            return out
+        }
+    },
+    'ncircles': {
+        'draw': (self) => {
+            doFill = false
+            doStroke = true
+            stroke('white')
+            strokeWeight(2)
+            let theta = Math.sin(PI / self.n)
+            let rad = theta / (1 + theta) * self.size
+            let radius = self.size - rad
+            for (let i = 0; i < self.n; i++) {
+                dir = i / self.n * PI * 2 + PI / 2 + PI / self.n
+                circle(Math.cos(dir) * radius, Math.sin(dir) * radius, rad)
+            }
+        },
+        'openings': (self, size) => {
+            if (self.n == 1) {
+                return [
+                    {
+                        'size': 0,
+                        'position': new Vector(0, 0),
+                        'rotation': 0,
+                        'ocu': [0]
+                    },
+                    {
+                        'size': size,
+                        'position': new Vector(0, 0),
+                        'rotation': 0,
+                        'ocu': [1]
+                    }
+                ]
+            }
+            let theta = Math.sin(PI / self.n)
+            let rad = theta / (1 + theta) * self.size
+            let radius = self.size - rad
+            let circles = []
+            for (let i = 0; i < self.n; i++) {
+                dir = i / self.n * PI * 2 + PI / 2 + PI / self.n
+                circles.push([new Vector(Math.cos(dir) * radius, Math.sin(dir) * radius), dir + PI / 2])
+            }
+            let out = [
+                {
+                    'size': size - (2 * rad),
+                    'position': new Vector(0, 0),
+                    'rotation': 0,
+                    'ocu': [0]
+                }
+            ]
+            for (let circle of circles) {
+                out.push({
+                    'size': rad,
+                    'position': circle[0],
+                    'rotation': circle[1],
+                    'ocu': [out.length]
+                })
+            }
+            return out
+        }
+    },
+    'scale': {
+        'draw': (self) => {},
+        'openings': (self, size) => {
+            return [{
+                'size': size * self.n,
+                'position': new Vector(0, 0),
+                'rotation': 0,
+                'ocu': [0]
+            }]
+        }
+    },
+    'rotate': {
+        'draw': (self) => {},
+        'openings': (self, size) => {
+            return [{
+                'size': size,
+                'position': new Vector(0, 0),
+                'rotation': self.n,
+                'ocu': [0]
+            }]
+        }
+    },
+    'spin': {
+        'draw': (self) => {
+            self.openings = TYPES[self.type].openings(self, self.size)
+            self.children.forEach((v) => {
+                v.recalc()
+            })
+        },
+        'openings': (self, size) => {
+            return [{
+                'size': size,
+                'position': new Vector(0, 0),
+                'rotation': (self.n * time) / 180 * PI,
+                'ocu': [0]
+            }]
+        }
+    }
+}
+
+class AlchemyCircle extends Entity {
+    constructor(type, parent, pos = 0, ...args) {
+        let name = 'root'
+        if (parent) name = parent.name + ':0'
+        super(name, TYPES[type].draw)
+        this.name = name
+        this.pos = pos
+        this.parent = parent
+        if (['ngon', 'ncircles', 'scale', 'rotate', 'spin'].includes(type)) this.n = args[0] || 3
+        if (['ngon'].includes(type)) {
+            this.j = args[1] || 1
+            if (this.j > this.n / 2) this.j = this.n - this.j
+        }
+        if (parent) {
+            this._size = parent.openings[pos].size
+            this.position = parent.openings[pos].position
+            this.rotation = parent.openings[pos].rotation
+            if (parent.open[pos]) {
+                parent.add_child(this)
+                for (let point of parent.openings[pos].ocu) {
+                    parent.open[point] = false
+                }
+            }
+        } else {
+            this._size = Math.min(canvas.width, canvas.height) / 2
+            this.position = new Vector(canvas.width / 2, canvas.height / 2)
+        }
+        this.openings = TYPES[type].openings(this, this.size)
+        this.open = []
+        this.type = type
+        for (let i = 0; i < this.openings.length; i++) {
+            this.open[i] = true
+        }
+    }
+
+    set size(v) {
+        this.openings = TYPES[this.type].openings(this, v)
+        this.children.forEach((v) => {
+            v.recalc()
+        })
+        this._size = v
+    }
+
+    get size() {
+        return this._size
+    }
+
+    recalc() {
+        this._size = this.parent.openings[this.pos].size
+        this.position = this.parent.openings[this.pos].position
+        this.rotation = this.parent.openings[this.pos].rotation
+    }
+}
---
+++ cool circles/main.js
@@ -0,0 +1,222 @@
+let selected = 0
+let selectedValue = null
+let scroll = 0
+let argumentIndex = 1
+let time = 0
+
+function start() {
+    data.rootCircle = new AlchemyCircle('circle')
+    data.rootCircle.add()
+    data.tree = [
+        [
+            null,
+            true,
+            null
+        ]
+    ]
+    parseTree()
+}
+
+async function tick(delta) {
+    canvas.width = canvas.screenWidth
+    canvas.height = canvas.screenHeight
+    if (canvas.width / 1.5 < canvas.height) {
+        data.panelX = canvas.width / 1.5
+        data.panelXS = canvas.width / 3
+    } else {
+        data.panelX = canvas.height
+        data.panelXS = canvas.width - canvas.height
+    }
+    data.rootCircle.size = data.panelX / 2
+    data.rootCircle.position = new Vector(data.panelX / 2, canvas.height / 2)
+    background(0)
+    doFill = true
+    doStroke = true
+    fill(90)
+    stroke(135)
+    strokeWeight(4)
+    rect(data.panelX, 0, data.panelXS, canvas.height / 2, 1)
+    rect(data.panelX, canvas.height / 2, data.panelXS, canvas.height / 2, 1)
+    render()
+    let options = [
+        'circle',
+        'ngon',
+        'ncircles',
+        'spread',
+        'scale',
+        'rotate',
+        'spin',
+        'none'
+    ]
+    options.forEach((v, i) => {
+        doFill = true
+        doStroke = false
+        fill(0)
+        if (
+            input.mouseX >= data.panelX + 5 &&
+            input.mouseX <= data.panelX + data.panelXS - 5 &&
+            input.mouseY >= 5 + i * 25 + canvas.height / 2 &&
+            input.mouseY <= 25 + i * 25 + canvas.height / 2
+        ) {
+            fill(31)
+            if (input.isButtonPressed('LeftMouseButton')) {
+                selectedValue = [v]
+            }
+        }
+        rect(data.panelX + 5, 5 + i * 25 + canvas.height / 2, data.panelXS - 10, 20, 1)
+        if (v) {
+            fill(255)
+            text(v, data.panelX + 7, 18 + i * 25 + canvas.height / 2)
+        }
+    })
+    button('copy', () => {
+        input.writeClipboard(JSON.stringify(data.tree))
+    }, data.panelX + 7, canvas.height - 50, (data.panelXS / 2) - 10, 20)
+    button('paste', async () => {
+        let tree = await input.readClipboard()
+        if (tree) data.tree = JSON.parse(tree)
+    }, data.panelX + 2 + (data.panelXS / 2), canvas.height - 50, (data.panelXS / 2) - 10, 20)
+    button('save', () => {
+        input.saveFile(JSON.stringify(data.tree), 'circle.json')
+    }, data.panelX + 7, canvas.height - 25, (data.panelXS / 2) - 10, 20)
+    button('load', async () => {
+        let tree = await input.loadFile()
+        tree = await input.readFile(tree)
+        if (tree) data.tree = JSON.parse(tree)
+    }, data.panelX + 2 + (data.panelXS / 2), canvas.height - 25, (data.panelXS / 2) - 10, 20)
+    if (input.isButtonPressed('ArrowUp') && selected > 0) selected--
+    if (input.isButtonPressed('ArrowDown')) selected++
+    if (input.isButtonPressed('ArrowLeft') && argumentIndex > 1) argumentIndex--
+    if (input.isButtonPressed('ArrowRight')) argumentIndex++
+    parseTree()
+    time += delta
+}
+
+function render(arr = data.tree, ind = 1, origin = 0, path = []) {
+    let i = -1
+    arr.forEach((v, j) => {
+        if (path.length == 0 || v[1] || v[0] != null) {
+            i++
+            doFill = true
+            doStroke = false
+            fill(0)
+            if (
+                input.mouseX >= data.panelX + ind * 5 &&
+                input.mouseX <= data.panelX + ind * 5 + data.panelXS - (ind + 1) * 5 &&
+                input.mouseY >= 5 + (i + origin - scroll) * 25 &&
+                input.mouseY <= 25 + (i + origin - scroll) * 25 &&
+                25 + (i + origin - scroll) * 25 < canvas.height / 2
+            ) {
+                fill(31)
+                if (input.isButtonPressed('LeftMouseButton')) {
+                    selected = i + origin
+                    argumentIndex = 1
+                }
+            }
+            if (selected == (i + origin)) {
+                fill(63)
+                let a = data.tree
+                for (let index of path) {
+                    a = a[index][2]
+                }
+                if (selectedValue != null) {
+                    a[j][0] = selectedValue
+                    a[j][2] = []
+                    if (selectedValue[0] == 'none') a[j] = [null, true, null]
+                    selectedValue = null
+                }
+                if (a[j][0] && !a[j][0][argumentIndex]) a[j][0][argumentIndex] = ''
+                if (a[j][0]) a[j][0][argumentIndex] += input.keysdown.filter((v) => '0123456789.-'.split('').includes(v)).join('')
+                if (a[j][0] && input.isKeyPressed('Backspace')) a[j][0][argumentIndex] = a[j][0][argumentIndex].slice(0, -1)
+                while (25 + (i + origin - scroll) * 25 > canvas.height / 2) scroll++
+                while (5 + (i + origin - scroll) * 25 < 0) scroll--
+            }
+            if (25 + (i + origin - scroll) * 25 < canvas.height / 2) {
+                rect(data.panelX + ind * 5, 5 + (i + origin - scroll) * 25, data.panelXS - (ind + 1) * 5, 20, 1)
+                fill(255)
+                if (v[0]) {
+                    while ((v[0].length > 1 && v[0][v[0].length - 1] == '') && !(v[0].length - 1 <= argumentIndex && (i + origin) == selected)) {
+                        v[0].pop();
+                    }
+                    v[0] = Array.from(v[0], item => item === undefined ? '' : item)
+                    text(j + ': ' + v[0].map((a, b) => b == argumentIndex && (i + origin) == selected ? '_' + a + '_' : a).map((a, b) => b != 0 ? b + ': ' + a : a).join(', '), data.panelX + ind * 5 + 2, 18 + (i + origin - scroll) * 25)
+                    i += render(v[2], ind + 1, i + origin + 1, [...path, j])
+                } else {
+                    text(j, data.panelX + ind * 5 + 2, 18 + (i + origin - scroll) * 25)
+                }
+            }
+        }
+    })
+    return i + 1
+}
+
+function parseTree(arr = [data.tree[0]], origin = data.rootCircle, path = [], stop = []) {
+    origin.children.length = 0
+    origin.open = origin.open.map(() => true)
+    arr.forEach((v, i) => {
+        if (v != null && v[0] != null) {
+            if (v[0][0] == 'spread') {
+                let num = Number(v[0][1]) || 1
+                if (stop.includes(num)) return
+                if (!data.tree[num]) data.tree[num] = [null, true, null]
+                let val = data.tree[num]
+                if (val != null && val[0] != null) {
+                    let circle = new AlchemyCircle(val[0][0], origin, i, ...val[0].slice(1))
+                    let a = data.tree
+                    if (a[num][2] instanceof Array) {
+                        for (let n = 0; n < a.length; n++) {
+                            a[n][1] = origin.open[n]
+                        }
+                        while (circle.openings.length > a[num][2].length) {
+                            a[num][2].push([null, true, null])
+                        }
+                        while (circle.openings.length < a[num][2].length) {
+                            a[num][2].pop()
+                        }
+                    }
+                    parseTree(val[2], circle, [num], [...stop, num])
+                }
+            } else {
+                let circle = new AlchemyCircle(v[0][0], origin, i, ...v[0].slice(1).map((value) => Number(value)))
+                if (arr[i][2] instanceof Array) {
+                    let a = data.tree
+                    for (let index of path) {
+                        a = a[index][2]
+                    }
+                    for (let n = 0; n < a.length; n++) {
+                        a[n][1] = origin.open[n]
+                    }
+                    while (circle.openings.length > a[i][2].length) {
+                        a[i][2].push([null, true, null])
+                    }
+                    while (circle.openings.length < a[i][2].length) {
+                        a[i][2].pop()
+                    }
+                }
+                parseTree(v[2], circle, [...path, i], stop)
+            }
+        }
+    })
+}
+
+function button(label, callback, x, y, xSize, ySize) {
+    doFill = true
+    doStroke = false
+    fill(0)
+    if (
+        input.mouseX >= x &&
+        input.mouseX <= x + xSize &&
+        input.mouseY >= y &&
+        input.mouseY <= y + ySize
+    ) {
+        fill(31)
+        if (input.isButtonPressed('LeftMouseButton')) {
+            fill(63)
+            callback()
+        }
+    }
+    rect(x, y, xSize, ySize, 1)
+    fill(255)
+    text(label, x + 2, y + 12)
+}
+//*/
