ba8e14341fb3c78a6a9ff9b457223fa9a134bf69
morkley1 <63617880+morkley1@users.noreply.github.com>
2025-07-10 13:52:57 -0400

    Fix Button

--- cool circles/main.js
+++ cool circles/main.js
@@ -85,9 +85,9 @@ async function tick(delta) {
         tree = await input.readFile(tree)
         if (tree) data.tree = JSON.parse(tree)
     }, data.panelX + 2 + (data.panelXS / 2), canvas.height - 25, (data.panelXS / 2) - 10, 20)
-    button('scroll direction: ' + (scrollDirection == 1 ? 'up' : 'down'), async () => {
+    button('scroll direction: ' + (scrollDirection == 1 ? 'down' : 'up'), async () => {
         scrollDirection = -scrollDirection
-    }, data.panelX + 7, canvas.height - 75, (data.panelXS / 2) - 10, 20)
+    }, data.panelX + 7, canvas.height - 75, data.panelXS - 10, 20)
     if (input.isButtonPressed('ArrowUp') && selected > 0) {
         selected--
         while (25 + (selected - Math.floor(scroll)) * 25 > canvas.height / 2) scroll++
